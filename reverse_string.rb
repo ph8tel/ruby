615e309430a510354789b7a1df51ff4133049030
ph8l <ph8tel@gmail.com>
2016-11-01 16:35:52 -0700

    added logic to determine longest word in given string

---
+++ reverse_string.rb
@@ -0,0 +1,32 @@
+# Write a method that will take a string as input, and return a new
+# string with the same letters in reverse order.
+#
+# Don't use String's reverse method; that would be too simple.
+#
+# Difficulty: easy.
+
+def reverse(string)
+	answer = String.new
+	i = string.length - 1
+	while i >= 0 
+	answer = answer + string[i]
+	i -= 1 
+end
+p answer
+end
+
+# These are tests to check that your code is working. After writing
+# your solution, they should all print true.
+
+puts("\nTests for #reverse")
+puts("===============================================")
+    puts(
+      'reverse("abc") == "cba": ' + (reverse("abc") == "cba").to_s
+    )
+    puts(
+      'reverse("a") == "a": ' + (reverse("a") == "a").to_s
+    )
+    puts(
+      'reverse("") == "": ' + (reverse("") == "").to_s
+    )
+puts("===============================================")
