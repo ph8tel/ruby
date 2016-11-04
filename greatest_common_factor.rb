ca49107ef4c44346caad126c01b8c87180469cf2
ph8l <ph8tel@gmail.com>
2016-11-04 09:26:35 -0700

    added some comments about logic flow

--- greatest_common_factor.rb
+++ greatest_common_factor.rb
@@ -1,9 +1,3 @@
-# Write a method that takes in two numbers. Return the greatest
-# integer that evenly divides both numbers. You may wish to use the
-# `%` modulo operation.
-#
-# Difficulty: medium.
-
 def greatest_common_factor(number1, number2)
 	answer = 0
 	if number1 > number2
@@ -14,30 +8,17 @@ def greatest_common_factor(number1, number2)
 	
 	while r > 0  
 		if (number1 % r == 0) && (number2 % r == 0) 
-			puts "found at #{r}"
 			return r 
 		end
 		r -= 1
 	end
-	
 
 end
 
-# These are tests to check that your code is working. After writing
-# your solution, they should all print true.
+puts "This logic will find the greatest common factor between two numbers"
+puts "Enter first number:"
+a = Integer(gets)
+puts "Enter second number:"
+b = Integer(gets)
 
-puts("\nTests for #greatest_commmon_factor")
-puts("===============================================")
-    puts(
-      'greatest_common_factor(3, 9) == 3: ' +
-      (greatest_common_factor(3, 9) == 3).to_s
-    )
-    puts(
-      'greatest_common_factor(16, 24) == 8: ' +
-      (greatest_common_factor(16, 24) == 8).to_s
-    )
-    puts(
-      'greatest_common_factor(3, 5) == 1: ' +
-      (greatest_common_factor(3, 5) == 1).to_s
-    )
-puts("===============================================")
+p greatest_common_factor(a, b)
