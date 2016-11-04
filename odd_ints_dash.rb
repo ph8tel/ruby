5e266116064d2583892a960768f58cd05aed00be
ph8l <ph8tel@gmail.com>
2016-11-04 12:01:42 -0700

    Added some comments

--- odd_ints_dash.rb
+++ odd_ints_dash.rb
@@ -1,12 +1,3 @@
-# Write a method that takes in a number and returns a string, placing
-# a single dash before and after each odd digit. There is one
-# exception: don't start or end the string with a dash.
-#
-# You may wish to use the `%` modulo operation; you can see if a number
-# is even if it has zero remainder when divided by two.
-#
-# Difficulty: medium.
-
 def dasherize_number(num)
 	output = num.to_s.split('')
 	idx = 0 
@@ -24,28 +15,17 @@ def dasherize_number(num)
 	answer = output.join
 	answer.gsub!('--', '-')
 
-	p answer
+answer
 end
-# new note
-# These are tests to check that your code is working. After writing
-# your solution, they should all print true.
-puts "joe test #{dasherize_number(8762348273648123761823764)}"
-puts("\nTests for #dasherize_number")
-puts("===============================================")
-    puts(
-      'dasherize_number(203) == "20-3": ' +
-      (dasherize_number(203) == '20-3').to_s
-    )
-    puts(
-      'dasherize_number(303) == "3-0-3": ' +
-      (dasherize_number(303) == '3-0-3').to_s
-    )
-    puts(
-      'dasherize_number(333) == "3-3-3": ' +
-      (dasherize_number(333) == '3-3-3').to_s
-    )
-    puts(
-      'dasherize_number(3223) == "3-22-3": ' +
-      (dasherize_number(3223) == '3-22-3').to_s
-    )
-puts("===============================================")
+
+
+puts "This logic places dashes around odd numbers"
+puts "Please enter a number to test: "
+num = Integer(gets) rescue false
+while !num 
+  puts "Please try again, with a NUMBER"
+  num = Integer(gets) rescue false
+end
+
+p dasherize_number(num)
+
--- reverse_string.rb
+++ reverse_string.rb
@@ -1,10 +1,3 @@
-# Write a method that will take a string as input, and return a new
-# string with the same letters in reverse order.
-#
-# Don't use String's reverse method; that would be too simple.
-#
-# Difficulty: easy.
-
 def reverse(string)
 	answer = String.new
 	i = string.length - 1
@@ -12,21 +5,11 @@ def reverse(string)
 	answer = answer + string[i]
 	i -= 1 
 end
-p answer
+
+answer
 end
 
-# These are tests to check that your code is working. After writing
-# your solution, they should all print true.
-
-puts("\nTests for #reverse")
-puts("===============================================")
-    puts(
-      'reverse("abc") == "cba": ' + (reverse("abc") == "cba").to_s
-    )
-    puts(
-      'reverse("a") == "a": ' + (reverse("a") == "a").to_s
-    )
-    puts(
-      'reverse("") == "": ' + (reverse("") == "").to_s
-    )
-puts("===============================================")
+puts "This logic is just my version of the .reverse method"
+puts "Please enter a string to reverse:"
+str = gets.chomp
+p reverse(str)
