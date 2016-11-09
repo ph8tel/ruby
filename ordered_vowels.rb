fee46eeeb13cafa15ea0da0fbfa0ec0d3ba5374d
ph8l <ph8tel@gmail.com>
2016-11-09 13:20:20 -0800

    replaced testing code and cleaned up spacing

--- ordered_vowels.rb
+++ ordered_vowels.rb
@@ -1,6 +1,5 @@
 # github.com/ph8tel
 
-
 # The method `ordered_vowel_words(str)`  takes a string of
 # lowercase words and returns a string with just the words containing
 # all their vowels (excluding "y") in alphabetical order. Vowels may
@@ -8,16 +7,16 @@
 
 def ordered_vowel_words(str)
 	r = Array.new
-a = str.split(' ')
-a.each {|word| r << word if ordered_vowel_word?(word)}
-r.join(' ')
+	a = str.split(' ')
+	a.each {|word| r << word if ordered_vowel_word?(word)}
+	r.join(' ')
 end
 
 def ordered_vowel_word?(word)
-a = (word.split('').select { |v| v =~ /[aeiou]/ }) 
+	a = (word.split('').select { |v| v =~ /[aeiou]/ }) 
 # (word.split('').select { |v| v =~ /[aeiou]/ }).sort! ? true : false
 
-a == a.sort
+	a == a.sort
 end
 
 #p ordered_vowel_words('raeiou aoiurtea eio euo wert slkd')
