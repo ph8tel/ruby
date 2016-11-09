e8fb82ce134ba9bc384a5e25a626f28c5f3a7727
ph8l <ph8tel@gmail.com>
2016-11-09 13:39:39 -0800

    Fixed rspec test #4 fail by changing return format

--- area_overlap.rb
+++ area_overlap.rb
@@ -6,11 +6,11 @@
 # bottom-left and top-right coordinates (given in `[x, y]` notation).
 
 def rec_intersection(rect1, rect2)
+	xmin = rect1[0][0] > rect2[0][0] ? rect1[0][0] : rect2[0][0]
+	xmax = rect1[1][0] > rect2[1][0] ? rect2[1][0] : rect1[1][0]
+	ymin = rect1[0][1] > rect2[0][1] ? rect1[0][1] : rect2[0][1]
+	ymax = rect1[1][1] > rect2[1][1] ? rect2[1][1] : rect1[1][1]
 
-xmin = rect1[0][0] > rect2[0][0] ? rect1[0][0] : rect2[0][0]
-xmax = rect1[1][0] > rect2[1][0] ? rect2[1][0] : rect1[1][0]
-ymin = rect1[0][1] > rect2[0][1] ? rect1[0][1] : rect2[0][1]
-ymax = rect1[1][1] > rect2[1][1] ? rect2[1][1] : rect1[1][1]
 return nil if ((xmax < xmin) || (ymax < ymin))
 return [ [xmin, ymin], [xmax, ymax]  ]
 end
--- morse.rb
+++ morse.rb
@@ -5,13 +5,25 @@
 # two spaces between words and one space between letters.
 #
 
-def translate(str)
+def translate(letter)
 	morse_dict = {"a" => ".-", "b" => "-...", "c" => "-.-.", "d" => "-..", "e" => ".", "f" => "..-.", "g" => "--.",	"h" => "....","i" => "..",	"j" => ".---",	"k" => "-.-",	"l" => ".-..","m" => "--",	"n" => "-.",	"o" => "---",	"p" => ".--.",	"q" => "--.-",	"r" => ".-.",	"s" => "...",	"t" => "-",	"u" => "..-",	"v" => "...-",	"w" => ".--",	"x" => "-..-",	"y" => "-.--",	"z" => "--..",	" " => " ",	"1" => ".----",	"2" => "..---",	"3" => "...--",	"4" => "....-",	"5" => ".....",	"6" => "-....",	"7" => "--...",	"8" => "---..",	"9" => "----.",	"0" => "-----"}
 	morse_dict[letter]
 end
 
-def morse_encode(letter)
-	str.split('').map! { |letter| morse(letter) }.join(' ')	
+def morse_encode(str)
+
+	a = str.delete(' ').split('')
+	p a
+
+	a.map! do
+		 |letter| 
+		 translate(letter) + " "
+		end
+		p a
+		 r = a.join(' ')
+		 e = r[0..-2]
+		 p e
+
 end
 
-
+morse_encode("the cat in the hat")
