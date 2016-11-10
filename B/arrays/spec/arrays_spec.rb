eb8d4b50d5d221ee7ff972e03d843bfdaf09fee8
ph8l <ph8tel@gmail.com>
2016-11-09 22:17:41 -0800

    4 respec tests written and passed for zero_sum method

--- B/arrays/spec/arrays_spec.rb
+++ B/arrays/spec/arrays_spec.rb
@@ -5,20 +5,19 @@ describe "#my_uniq" do
   it "removes duplicates from array" do
     my_uniq([1, 2, 1, 3, 3]).should == [1, 2, 3]
   end
+end
 
 describe "#zero_sum" do
   it "check to see if 2 add up to 0" do
-    my_uniq([1, 2, 1, 3, 3]).should == false
+    zero_sum([1, 2, 1, 3, 3]).should == false
   end
 
-describe "#zero_sum" do
   it "check to see if 2 add up to 0" do
-    my_uniq([1, 2, 1, 3, -1]).should == true
+    zero_sum([1, 2, 1, 3, -1]).should == true
   end
 
-describe "#zero_sum" do
   it "check to see if 2 add up to 0" do
-    my_uniq([1, 2, 1, 3, 0, 0]).should == true
+    zero_sum([1, 2, 1, 3, 0, 0]).should == true
   end
 
 
